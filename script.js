1a2b1502cf7329129a196232a6e3ff3d96817c6e
MattiaToscano <mattia1997ct@gmail.com>
2025-03-26 14:31:57 +0100

    [UPD] Recuperati elementi DOM

---
+++ script.js
@@ -0,0 +1,16 @@
+//Milestone 2
+// Recupero tutti gli elementi del DOM che occorrono
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const form = document.getElementById('form');
+const nome = document.getElementById('nome-cognome');
+const km = document.getElementById('km');
+const eta = document.getElementById('eta');
+
+//recupero campi card
+const offerta = document.getElementById('offerta');
+const carrozza = document.getElementById('carrozza');
+const codice = document.getElementById('codice');
+const costo = document.getElementById('costo');
+
+//
