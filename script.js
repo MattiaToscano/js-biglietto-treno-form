34c926da9acdce1e602d417e223af4a862d015ae
MattiaToscano <mattia1997ct@gmail.com>
2025-03-26 14:43:45 +0100

    [UPD] Check campi compilati

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Recupero tutti gli elementi del DOM che occorrono
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
-const form = document.getElementById('form');
+const form = document.querySelector('form');
 const nome = document.getElementById('nome-cognome');
 const km = document.getElementById('km');
 const eta = document.getElementById('eta');
@@ -13,4 +13,12 @@ const carrozza = document.getElementById('carrozza');
 const codice = document.getElementById('codice');
 const costo = document.getElementById('costo');
 
-//
+//Creo l'azione click del bottone
+button1.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    //Controllo se i campi sono stati compilati
+    carrozza.innerHTML = Math.floor(Math.random() * 9) + 1;
+    codice.innerHTML = Math.floor(Math.random() * 10000) + 90000;
+
+}
+);  
